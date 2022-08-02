2ee5abb64cd778e399af096c9473efdef099bfa5
Aleksey N <nester.a.l@yandex.ru>
2022-08-02 16:19:37 +0300

    базовые сущности приложения

---
+++ Common/PurchDep.Domain/Supplier.cs
@@ -0,0 +1,12 @@
+﻿using PurchDep.Domain.Base;
+
+namespace PurchDep.Domain
+{
+    public class Supplier<T> : ISupplier<T>
+    {
+        public HashSet<IProduct<T>> Products { get; set; } = new();
+        public T Id { get; set; } = default(T)!;
+        public string Name { get; set; } = string.Empty;
+    }
+    public class Supplier : Supplier<int>, ISupplier { }
+}
