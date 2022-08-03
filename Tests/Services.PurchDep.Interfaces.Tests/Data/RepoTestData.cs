a3e103b7d6b5dcca0a8940634a5176f3d9346467
Aleksey N <nester.a.l@yandex.ru>
2022-08-03 18:10:07 +0300

    репозиторий поставщика + тестирование

--- Tests/Services.PurchDep.Interfaces.Tests/Data/RepoTestData.cs
+++ Tests/Services.PurchDep.Interfaces.Tests/Data/RepoTestData.cs
@@ -13,16 +13,23 @@ namespace Services.PurchDep.Interfaces.Tests.Data
         public static Product Product4 { get; } = new Product() { Name = "Product_4", Price = 4.44m, };
         public static Product Product5 { get; } = new Product() { Name = "Product_5", Price = 5.55m, };
         public static Product Product6 { get; } = new Product() { Name = "Product_6", Price = 6.66m, };
-        public static Supplier Supplier1 { get; } = new Supplier() { Id = 1, Name = "Supplier_1" };
-        public static Supplier Supplier2 { get; } = new Supplier() { Id = 2, Name = "Supplier_2" };
+        public static Supplier Supplier1 { get; } = new Supplier() { Name = "Supplier_1" };
+        public static Supplier Supplier2 { get; } = new Supplier() { Name = "Supplier_2" };
+        public static Supplier Supplier3 { get; } = new Supplier() { Name = "Supplier_3" };
+        public static Supplier Supplier4 { get; } = new Supplier() { Name = "Supplier_4" };
+        public static Supplier Supplier5 { get; } = new Supplier() { Name = "Supplier_5" };
+        public static Supplier Supplier6 { get; } = new Supplier() { Name = "Supplier_6" };
         static RepoTestData()
         {
             AllProducts.Add(Product1);
             AllProducts.Add(Product2);
 
             Supplier1.Products.Add(Product1);
-            Supplier2.Products.Add(Product1);
             Supplier2.Products.Add(Product2);
+            Supplier3.Products.Add(Product3);
+            Supplier4.Products.Add(Product4);
+            Supplier5.Products.Add(Product5);
+            Supplier6.Products.Add(Product6);
 
             AllSuppliers.Add(Supplier1);
             AllSuppliers.Add(Supplier2);
