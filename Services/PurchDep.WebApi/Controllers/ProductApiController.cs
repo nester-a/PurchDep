70b670667d7f4b562983011cf6bfc62579f6de8a
Aleksey N <nester.a.l@yandex.ru>
2022-08-04 18:14:21 +0300

    initial

---
+++ Services/PurchDep.WebApi/Controllers/ProductApiController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PurchDep.Dal.Entities;
+using PurchDep.Domain.Base;
+using PurchDep.Interfaces.Base.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurchDep.WebApi.Controllers
+{
+    [Route("/products")]
+    [ApiController]
+    public class ProductApiController : ControllerBase
+    {
+        private readonly Service<Product, IProduct> _service;
+
+        public ProductApiController(Service<Product, IProduct> service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var items = _service.GetAll();
+
+            if(!items.Any()) return NoContent();
+
+            return Ok(items);
+        }
+
+        [HttpGet("id:int")]
+        public IActionResult GetById(int id)
+        {
+            IProduct item;
+            try
+            {
+                item = _service.Get(id);
+            }
+            catch (ArgumentException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] IProduct item)
+        {
+            try
+            {
+                _service.Add(item);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Ok(item);
+        }
+
+        [HttpPut("id:int")]
+        public IActionResult Edit(int id, [FromBody] IProduct itemToEdit)
+        {
+            IProduct product;
+            try
+            {
+                product = _service.Update(id, itemToEdit);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return NotFound(ex.Message);
+            }
+
+            return Ok(product);
+        }
+
+        [HttpDelete("id:int")]
+        public IActionResult Delete(int id)
+        {
+            IProduct item;
+            try
+            {
+                item = _service.Delete(id);
+            }
+            catch (ArgumentException ex)
+            {
+                return NotFound(ex.Message);
+            }
+
+            return Ok(item);
+        }
+    }
+}
---
+++ Services/PurchDep.WebApi/Program.cs
@@ -0,0 +1,47 @@
+using PurchDep.Dal.Entities;
+using PurchDep.Domain.Base;
+using PurchDep.Interfaces.Base.Mapping;
+using PurchDep.Interfaces.Base.Services;
+using PurchDep.Interfaces.Mapping;
+using PurchDep.Interfaces.Repositories;
+using PurchDep.Interfaces.Services;
+using PurchDep.WebApi.DependencyInjection;
+
+using ProductDom = PurchDep.Domain.Product;
+using SupplierDom = PurchDep.Domain.Supplier;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+// Add services to the container.
+builder.Services.AddAppContext(configuration);
+builder.Services.AddScoped<IProduct, ProductDom>();
+builder.Services.AddScoped<ISupplier, SupplierDom>();
+builder.Services.AddScoped<IMappingService<Product, IProduct>, ProductMappingService>();
+builder.Services.AddScoped<IMappingService<Supplier, ISupplier>, SupplierMappingService>();
+builder.Services.AddScoped<IRepository<Product>, ProductRepository>();
+//builder.Services.AddScoped<Repository<Product>, ProductRepository>();
+builder.Services.AddScoped<IRepository<Supplier>, SupplierRepository>();
+//builder.Services.AddScoped<Repository<Supplier>, SupplierRepository>();
+builder.Services.AddScoped<IService<IProduct>, ProductService>();
+builder.Services.AddScoped<IService<ISupplier>, SupplierService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
