de782af0fedaa099d56257ff7ce7375213c643a3
Aleksey N <nester.a.l@yandex.ru>
2022-08-07 16:49:04 +0300

    рефакторинг сервиса для работы с товарами (проходит тесты)

--- Services/PurchDep.WebApi/Controllers/ProductApiController.cs
+++ Services/PurchDep.WebApi/Controllers/ProductApiController.cs
@@ -2,94 +2,14 @@
 using PurchDep.Dal.Entities;
 using PurchDep.Domain.Base;
 using PurchDep.Interfaces.Base.Services;
+using PurchDep.WebApi.Controllers.Base;
 
 namespace PurchDep.WebApi.Controllers
 {
     [Route("api/products")]
     [ApiController]
-    public class ProductApiController : ControllerBase
+    public class ProductApiController : PurchDepBaseController<Product, IProduct>
     {
-        private readonly Service<Product, IProduct> _service;
-
-        public ProductApiController(Service<Product, IProduct> service)
-        {
-            _service = service;
-        }
-
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            var items = _service.GetAll();
-
-            if (!items.Any()) return NoContent();
-
-            return Ok(items);
-        }
-
-        [HttpGet("id")]
-        public IActionResult GetById(int id)
-        {
-            IProduct item;
-            try
-            {
-                item = _service.Get(id);
-            }
-            catch (ArgumentException ex)
-            {
-                return NotFound(ex.Message);
-            }
-            return Ok(item);
-        }
-
-        [HttpPost]
-        public IActionResult Add([FromBody] IProduct item)
-        {
-            try
-            {
-                _service.Add(item);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-
-            return Ok(item);
-        }
-
-        [HttpPut("id")]
-        public IActionResult Edit(int id, [FromBody] IProduct itemToEdit)
-        {
-            IProduct product;
-            try
-            {
-                product = _service.Update(id, itemToEdit);
-            }
-            catch (ArgumentNullException ex)
-            {
-                return BadRequest(ex.Message);
-            }
-            catch (ArgumentException ex)
-            {
-                return NotFound(ex.Message);
-            }
-
-            return Ok(product);
-        }
-
-        [HttpDelete("id")]
-        public IActionResult Delete(int id)
-        {
-            IProduct item;
-            try
-            {
-                item = _service.Delete(id);
-            }
-            catch (ArgumentException ex)
-            {
-                return NotFound(ex.Message);
-            }
-
-            return Ok(item);
-        }
+        public ProductApiController(Service<Product, IProduct> service) : base(service) { }
     }
 }
