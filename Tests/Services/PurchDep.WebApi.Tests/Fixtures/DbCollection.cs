05ed5d13751241cd8513b5811a3b32863c5d9c81
Aleksey N <nester.a.l@yandex.ru>
2022-08-07 15:07:18 +0300

    контроллер товаров + его тесты

---
+++ Tests/Services/PurchDep.WebApi.Tests/Fixtures/DbCollection.cs
@@ -0,0 +1,7 @@
+﻿using Xunit;
+
+namespace PurchDep.WebApi.Tests.Fixtures
+{
+    [CollectionDefinition("WebApi Database collection")]
+    public class ServiceDbCollection : ICollectionFixture<DbFixture> { }
+}
---
+++ Tests/Services/PurchDep.WebApi.Tests/Fixtures/DbFixture.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PurchDep.Dal;
+using PurchDep.WebApi.Tests.Data;
+
+namespace PurchDep.WebApi.Tests.Fixtures
+{
+    public class DbFixture
+    {
+        public PurchDepContext Db { get; private set; }
+
+        public DbFixture()
+        {
+            var builder = new DbContextOptionsBuilder<PurchDepContext>();
+            builder.UseInMemoryDatabase("Services.PurchDep.WebApi.Tests.InMemoryDb.Controller");
+            builder.EnableSensitiveDataLogging();
+
+            var options = builder.Options;
+
+            Db = new PurchDepContext(options);
+
+            Db.Database.EnsureDeleted();
+            Db.Database.EnsureCreated();
+
+            AddProducts();
+            AddSuppliers();
+            Db.SaveChanges();
+        }
+
+        private void AddProducts()
+        {
+            Db.Products.AddRange(TestData.AllProducts);
+        }
+
+        private void AddSuppliers()
+        {
+            Db.Suppliers.AddRange(TestData.AllSuppliers);
+        }
+        public void Dispose()
+        {
+            Db?.Dispose();
+        }
+    }
+}
