c82d48936143acdffad3973dae37c75fa6c8cca7
Aleksey N <nester.a.l@yandex.ru>
2022-08-08 11:21:17 +0300

    рефакторинг приспособы хоста

--- Tests/Services/PurchDep.WebApi.Clients.Tests.Integration.Tests/Fixtures/HostFixture.cs
+++ Tests/Services/PurchDep.WebApi.Clients.Tests.Integration.Tests/Fixtures/HostFixture.cs
@@ -1,16 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
 using System.Net.Http;
 
 namespace PurchDep.WebApi.Clients.Tests.Integration.Tests.Fixtures
 {
-    public class HostFixture
+    public class HostFixture : IDisposable
     {
+        bool _disposed;
         public WebApplicationFactory<Program> WebAPIHostBuilder { get; private set; }
         public HttpClient HttpClient { get; private set; }
         public HostFixture()
         {
             WebAPIHostBuilder = new WebApplicationFactory<Program>();
             HttpClient = WebAPIHostBuilder.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+            Dispose(true);
+            _disposed = true;
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+
+            if (disposing)
+            {
+                //освободить все управляемые ресурсы (удалить всё, что было создано в этом объекте)
+                HttpClient?.Dispose();
+                WebAPIHostBuilder?.Dispose();
+            }
+
+            //освобождение всех неуправляемых
         }
     }
 }
