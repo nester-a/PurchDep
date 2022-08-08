8afb517ed44b3ef7851c5176812c18d970f5974f
Aleksey N <nester.a.l@yandex.ru>
2022-08-08 11:14:47 +0300

    приспособы для тестирования клиента

---
+++ Tests/Services/PurchDep.WebApi.Clients.Tests.Integration.Tests/Fixtures/HostCollection.cs
@@ -0,0 +1,7 @@
+﻿using Xunit;
+
+namespace PurchDep.WebApi.Clients.Tests.Integration.Tests.Fixtures
+{
+    [CollectionDefinition("WebApi Client collection")]
+    public class HostCollection : ICollectionFixture<HostFixture> { }
+}
---
+++ Tests/Services/PurchDep.WebApi.Clients.Tests.Integration.Tests/Fixtures/HostFixture.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+
+namespace PurchDep.WebApi.Clients.Tests.Integration.Tests.Fixtures
+{
+    public class HostFixture
+    {
+        public WebApplicationFactory<Program> WebAPIHostBuilder { get; private set; }
+        public HttpClient HttpClient { get; private set; }
+        public HostFixture()
+        {
+            WebAPIHostBuilder = new WebApplicationFactory<Program>();
+            HttpClient = WebAPIHostBuilder.CreateClient();
+        }
+    }
+}
