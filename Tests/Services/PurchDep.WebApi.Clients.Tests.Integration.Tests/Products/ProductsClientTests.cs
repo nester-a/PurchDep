4c37620da9ef662cf08b2796563b07cccc0c45a8
Aleksey N <nester.a.l@yandex.ru>
2022-08-08 10:59:38 +0300

    заготовка интеграционных тестов клиента товаров

---
+++ Tests/Services/PurchDep.WebApi.Clients.Tests.Integration.Tests/Products/ProductsClientTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PurchDep.WebApi.Clients.Tests.Integration.Tests.Products
+{
+    public class ProductsClientTests
+    {
+        public ProductsClientTests()
+        {
+
+        }
+    }
+}
