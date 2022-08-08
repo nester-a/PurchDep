5b2073d0c3833327d6cf859b410b3d285410265e
Aleksey N <nester.a.l@yandex.ru>
2022-08-08 11:14:35 +0300

    небольшой рефакторинг для начала тестирования

--- Services/PurchDep.WebApi.Clients/Products/ProductsClient.cs
+++ Services/PurchDep.WebApi.Clients/Products/ProductsClient.cs
@@ -6,6 +6,6 @@ namespace PurchDep.WebApi.Clients.Products
 {
     public class ProductsClient : ClientBase<IProduct>, IService<IProduct>
     {
-        public ProductsClient(HttpClient client, string address) : base(client, "api/products") { }
+        public ProductsClient(HttpClient client) : base(client, "api/products") { }
     }
 }
