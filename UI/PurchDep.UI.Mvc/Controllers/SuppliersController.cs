3b88984af01d583a3db659483528c42fd2d22155
Aleksey N <nester.a.l@yandex.ru>
2022-08-09 17:48:58 +0300

    контроллер поставщиков и его частичные представления

---
+++ UI/PurchDep.UI.Mvc/Controllers/SuppliersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PurchDep.Domain;
+using PurchDep.Interfaces.Base.Services;
+
+namespace PurchDep.UI.Mvc.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        private readonly IService<Supplier> _service;
+
+        public SuppliersController(IService<Supplier> service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _service.GetAll();
+            return View(items);
+        }
+        public IActionResult Details(int id)
+        {
+            var item = _service.Get(id);
+            return View(item);
+        }
+        public IActionResult Delete(int id)
+        {
+            _service.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
