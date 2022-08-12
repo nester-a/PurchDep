b22947fe80965d46e9072ec0583d67f837db7458
Aleksey N <nester.a.l@yandex.ru>
2022-08-12 18:54:21 +0300

    тестируем сервис товаров с моком

--- Tests/Services/PurchDep.Interfaces.Tests/Services/Fixtures/ServiceFixture.cs
+++ Tests/Services/PurchDep.Interfaces.Tests/Services/Fixtures/ServiceFixture.cs
@@ -54,6 +54,7 @@ namespace PurchDep.Interfaces.Tests.Services.Fixtures
             ProductRepositoryMock.Setup(o => o.Get(It.IsAny<int>())).Returns(new ProductDal() { Id = 1, Name = "Getted_FromMockRepository_ProductObj" });
             ProductRepositoryMock.Setup(o => o.Delete(It.IsAny<int>())).Returns(new ProductDal() { Id = 1, Name = "Deleted_FromMockRepository_ProductObj" });
             ProductRepositoryMock.Setup(o => o.Update(It.IsAny<int>(), It.IsAny<ProductDal>())).Returns(new ProductDal() { Id = 1, Name = "Updated_FromMockRepository_ProductObj" });
+            ProductRepositoryMock.Setup(o => o.Add(null!)).Throws(new ArgumentNullException());
             ProductRepositoryMock.Setup(o => o.Add(It.IsAny<ProductDal>())).Returns(new ProductDal() { Id = 1, Name = "Added_FromMockRepository_ProductObj" });
             ProductRepositoryMockObject = ProductRepositoryMock.Object;
 
@@ -74,7 +75,10 @@ namespace PurchDep.Interfaces.Tests.Services.Fixtures
             StockRepositoryMockObject = StockRepositoryMock.Object;
 
             ProductMappingServiceMock = new Mock<ProductMappingService>();
+            ProductDal nullDal = null!;
+            ProductDom nullDom = null!;
             ProductMappingServiceMock.Setup(o => o.Map(It.IsAny<ProductDal>())).Returns(new ProductDom() { Id = 1, Name = "Mapped_FromMockMapper_ProductDomObj" });
+            ProductMappingServiceMock.Setup(o => o.Map(nullDal)).Returns(nullDom);
             ProductMappingServiceMock.Setup(o => o.Map(It.IsAny<ProductDom>())).Returns(new ProductDal() { Id = 1, Name = "Mapped_FromMockMapper_ProductDalObj" });
             ProductMappingServiceMockObject = ProductMappingServiceMock.Object;
 
--- Tests/Services/PurchDep.Interfaces.Tests/Services/ProductServiceTests.cs
+++ Tests/Services/PurchDep.Interfaces.Tests/Services/ProductServiceTests.cs
@@ -3,11 +3,6 @@ using PurchDep.Interfaces.Mapping;
 using PurchDep.Interfaces.Repositories;
 using PurchDep.Interfaces.Services;
 using PurchDep.Interfaces.Tests.Services.Fixtures;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 using ProductDal = PurchDep.Dal.Entities.Product;
 
@@ -31,12 +26,33 @@ namespace PurchDep.Interfaces.Tests.Services
         [Fact]
         public void AddNewItem()
         {
+            _productRepositoryMock.Setup(o => o.Add(It.IsAny<ProductDal>())).Returns(new ProductDal() { Id = 1, Name = "Added_FromMockRepository_ProductObj" });
             var res = _service.Add(new ProductDom());
 
             Assert.NotNull(res);
             Assert.NotEqual(0, res.Id);
             _productRepositoryMock.Verify(prm => prm.Add(It.IsAny<ProductDal>()));
             _productMappingServiceMock.Verify(pms => pms.Map(It.IsAny<ProductDom>()));
+        }
+
+        [Fact]
+        public void AddNewItem_Thrown_ArgumentNullException()
+        {
+            //Assert.Throws<ArgumentNullException>(() => _service.Add(null!));
+            //_productRepositoryMock.Verify(prm => prm.Add(null!));
+
+            //_productRepositoryMock.Setup(o => o.Add(null!)).Throws(new ArgumentNullException());
+            //bool cathced = false;
+            //try
+            //{
+            //    _service.Add(null);
+            //}
+            //catch (ArgumentNullException e)
+            //{
+            //    cathced = true;
+            //    Assert.True(e is not null);
+            //}
+            //Assert.True(cathced);
         }
     }
 }
