56943c91ee5da6e226e9b0382f4980a0bcde97d2
Aleksey N <nester.a.l@yandex.ru>
2022-08-12 10:33:09 +0300

    тестирование маппинг сервиса складских товаров

--- Tests/Services/PurchDep.Interfaces.Tests/TestData/TestData.cs
+++ Tests/Services/PurchDep.Interfaces.Tests/TestData/TestData.cs
@@ -46,10 +46,42 @@ namespace PurchDep.Interfaces.Tests.TestData
             SupplierId = SupplierDom_1.Id,
         };
 
+        public static StockDal StockDal_1 { get; } = new()
+        {
+            Name = "StockDal_1",
+        };
+        public static StocksProductDal StocksProductDal_1 { get; } = new()
+        {
+            Product = ProductDal_1,
+            ProductId = ProductDal_1.Id,
+            Supplier = SupplierDal_1,
+            SupplierId = SupplierDal_1.Id,
+            Stock = StockDal_1,
+            StockId = StockDal_1.Id,
+            Quantity = 10,
+        };
+
+        public static StockDom StockDom_1 { get; } = new()
+        {
+            Name = "StockDom_1",
+        };
+        public static StocksProductDom StocksProductDom_1 { get; } = new()
+        {
+            Id = ProductDom_1.Id,
+            Name = ProductDom_1.Name,
+            SupplierId = SupplierDom_1.Id,
+            SuppliersPrice = SuppliersProductDom_1.SuppliersPrice,
+            StockId = StockDom_1.Id,
+            Quantity = 10,
+        };
+
         static TestData()
         {
             SupplierDal_1.SuppliersProducts.Add(SuppliersProductDal_1);
             SupplierDom_1.SuppliersProducts.Add(SuppliersProductDom_1);
+
+            StockDal_1.StocksProducts.Add(StocksProductDal_1);
+            StockDom_1.StocksProducts.Add(StocksProductDom_1);
         }
     }
 }
