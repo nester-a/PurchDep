bac9ddec589cf2e0876b836682429f2f3957fd9d
Aleksey N <nester.a.l@yandex.ru>
2022-08-13 14:38:00 +0300

    завершение тестирования сервиса товаров с моком

--- Tests/Services/PurchDep.Interfaces.Tests/Services/Fixtures/ServiceFixture.cs
+++ Tests/Services/PurchDep.Interfaces.Tests/Services/Fixtures/ServiceFixture.cs
@@ -16,83 +16,62 @@ namespace PurchDep.Interfaces.Tests.Services.Fixtures
 {
     public class ServiceFixture : IDisposable
     {
-        List<ProductDal> Products;
-        List<SupplierDal> Suppliers;
-        List<StockDal> Stocks;
+        public List<ProductDal> ProductsDal { get; }
+        public List<ProductDom> ProductsDom { get; }
+        public List<SupplierDal> SuppliersDal { get; }
+        public List<SupplierDom> SuppliersDom { get; }
+        public List<StockDal> StocksDal { get; }
+        public List<StockDom> StocksDom { get; }
 
-        PurchDepContext PurchDepContextMockObject { get; }
 
         public Mock<ProductRepository> ProductRepositoryMock { get; }
-        public ProductRepository ProductRepositoryMockObject { get; }
         public Mock<SupplierRepository> SupplierRepositoryMock { get; }
-        public SupplierRepository SupplierRepositoryMockObject { get; }
         public Mock<StockRepository> StockRepositoryMock { get; }
-        public StockRepository StockRepositoryMockObject { get; }
 
         public Mock<ProductMappingService> ProductMappingServiceMock { get; }
-        public ProductMappingService ProductMappingServiceMockObject { get; }
         public Mock<SupplierMappingService> SupplierMappingServiceMock { get; }
-        public SupplierMappingService SupplierMappingServiceMockObject { get; }
         public Mock<StockMappingService> StockMappingServiceMock { get; }
-        public StockMappingService StockMappingServiceMockObject { get; }
 
         public ServiceFixture()
         {
-            Products = new();
-            Suppliers = new();
-            Stocks = new();
+            ProductsDal = new();
+            ProductsDom = new();
+            SuppliersDal = new();
+            SuppliersDom = new();
+            StocksDal = new();
+            StocksDom = new();
 
-            Products.Add(TestData.TestData.ProductDal_1);
-            Suppliers.Add(TestData.TestData.SupplierDal_1);
-            Stocks.Add(TestData.TestData.StockDal_1);
+            ProductsDal.Add(TestData.TestData.ProductDal_1);
+            ProductsDom.Add(TestData.TestData.ProductDom_1);
+            SuppliersDal.Add(TestData.TestData.SupplierDal_1);
+            SuppliersDom.Add(TestData.TestData.SupplierDom_1);
+            StocksDal.Add(TestData.TestData.StockDal_1);
+            StocksDom.Add(TestData.TestData.StockDom_1);
 
-            DbContextOptions<PurchDepContext> options = new DbContextOptions<PurchDepContext>();
-            PurchDepContextMockObject = new Mock<PurchDepContext>(options).Object;
+            var options = new DbContextOptions<PurchDepContext>();
+            var purchDepContextMockObject = new Mock<PurchDepContext>(options).Object;
 
-            ProductRepositoryMock = new Mock<ProductRepository>(PurchDepContextMockObject);
-            ProductRepositoryMock.Setup(o => o.GetAll()).Returns(Products);
-            ProductRepositoryMock.Setup(o => o.Get(It.IsAny<int>())).Returns(new ProductDal() { Id = 1, Name = "Getted_FromMockRepository_ProductObj" });
-            ProductRepositoryMock.Setup(o => o.Update(It.IsAny<int>(), It.IsAny<ProductDal>())).Returns(new ProductDal() { Id = 1, Name = "Updated_FromMockRepository_ProductObj" });
-            ProductRepositoryMockObject = ProductRepositoryMock.Object;
-
-            SupplierRepositoryMock = new Mock<SupplierRepository>(PurchDepContextMockObject);
-            SupplierRepositoryMock.Setup(o => o.GetAll()).Returns(Suppliers);
-            SupplierRepositoryMock.Setup(o => o.Get(It.IsAny<int>())).Returns(new SupplierDal() { Id = 1, Name = "Getted_FromMockRepository_SupplierObj" });
-            SupplierRepositoryMock.Setup(o => o.Delete(It.IsAny<int>())).Returns(new SupplierDal() { Id = 1, Name = "Deleted_FromMockRepository_SupplierObj" });
-            SupplierRepositoryMock.Setup(o => o.Update(It.IsAny<int>(), It.IsAny<SupplierDal>())).Returns(new SupplierDal() { Id = 1, Name = "Updated_FromMockRepository_SupplierObj" });
-            SupplierRepositoryMock.Setup(o => o.Add(It.IsAny<SupplierDal>())).Returns(new SupplierDal() { Id = 1, Name = "Added_FromMockRepository_SupplierObj" });
-            SupplierRepositoryMockObject = SupplierRepositoryMock.Object;
-
-            StockRepositoryMock = new Mock<StockRepository>(PurchDepContextMockObject);
-            StockRepositoryMock.Setup(o => o.GetAll()).Returns(Stocks);
-            StockRepositoryMock.Setup(o => o.Get(It.IsAny<int>())).Returns(new StockDal() { Id = 1, Name = "Getted_FromMockRepository_StockObj" });
-            StockRepositoryMock.Setup(o => o.Delete(It.IsAny<int>())).Returns(new StockDal() { Id = 1, Name = "Deleted_FromMockRepository_StockObj" });
-            StockRepositoryMock.Setup(o => o.Update(It.IsAny<int>(), It.IsAny<StockDal>())).Returns(new StockDal() { Id = 1, Name = "Updated_FromMockRepository_StockObj" });
-            StockRepositoryMock.Setup(o => o.Add(It.IsAny<StockDal>())).Returns(new StockDal() { Id = 1, Name = "Added_FromMockRepository_StockObj" });
-            StockRepositoryMockObject = StockRepositoryMock.Object;
+            ProductRepositoryMock = new Mock<ProductRepository>(purchDepContextMockObject);
+            SupplierRepositoryMock = new Mock<SupplierRepository>(purchDepContextMockObject);
+            StockRepositoryMock = new Mock<StockRepository>(purchDepContextMockObject);
 
             ProductMappingServiceMock = new Mock<ProductMappingService>();
 
             var SuppliersProductMappingServiceMockObject = new Mock<SuppliersProductMappingService>().Object;
-
             SupplierMappingServiceMock = new Mock<SupplierMappingService>(SuppliersProductMappingServiceMockObject);
-            SupplierMappingServiceMock.Setup(o => o.Map(It.IsAny<SupplierDal>())).Returns(new SupplierDom() { Id = 1, Name = "Mapped_FromMockMapper_SupplierDomObj" });
-            SupplierMappingServiceMock.Setup(o => o.Map(It.IsAny<SupplierDom>())).Returns(new SupplierDal() { Id = 1, Name = "Mapped_FromMockMapper_SupplierDalObj" });
-            SupplierMappingServiceMockObject = SupplierMappingServiceMock.Object;
 
             var StocksProductMappingServiceMockObject = new Mock<StocksProductMappingService>().Object;
-
             StockMappingServiceMock = new Mock<StockMappingService>(StocksProductMappingServiceMockObject);
-            StockMappingServiceMock.Setup(o => o.Map(It.IsAny<StockDal>())).Returns(new StockDom() { Id = 1, Name = "Mapped_FromMockMapper_StockDomObj" });
-            StockMappingServiceMock.Setup(o => o.Map(It.IsAny<StockDom>())).Returns(new StockDal() { Id = 1, Name = "Mapped_FromMockMapper_StockDalObj" });
-            StockMappingServiceMockObject = StockMappingServiceMock.Object;
         }
 
         public void Dispose()
         {
-            Products?.Clear();
-            Suppliers?.Clear();
-            Stocks?.Clear();
+            ProductsDal?.Clear();
+            ProductsDom?.Clear();
+            SuppliersDal?.Clear();
+            SuppliersDom?.Clear();
+            StocksDal?.Clear();
+            StocksDom?.Clear();
         }
     }
 }
--- Tests/Services/PurchDep.Interfaces.Tests/Services/ProductServiceTests.cs
+++ Tests/Services/PurchDep.Interfaces.Tests/Services/ProductServiceTests.cs
@@ -4,6 +4,7 @@ using PurchDep.Interfaces.Repositories;
 using PurchDep.Interfaces.Services;
 using PurchDep.Interfaces.Tests.Services.Fixtures;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -18,10 +19,13 @@ namespace PurchDep.Interfaces.Tests.Services
     {
         Mock<ProductRepository> _repositoryMock;
         Mock<ProductMappingService> _mappingServiceMock;
+        private readonly ServiceFixture _fixture;
+
         public ProductServiceTests(ServiceFixture fixture)
         {
             _repositoryMock = fixture.ProductRepositoryMock;
             _mappingServiceMock = fixture.ProductMappingServiceMock;
+            _fixture = fixture;
         }
 
         [Fact]
@@ -129,6 +133,214 @@ namespace PurchDep.Interfaces.Tests.Services
 
             Assert.Throws<ArgumentException>(() => service.Delete(id));
             _repositoryMock.Verify(prm => prm.Delete(id));
+        }
+
+        [Fact]
+        public async Task DeleteAsyncTest()
+        {
+            _mappingServiceMock.Setup(o => o.MapAsync(It.IsAny<ProductDal>(), It.IsAny<CancellationToken>())).ReturnsAsync(new ProductDom() { Id = 1 });
+            _repositoryMock.Setup(o => o.DeleteAsync(It.IsAny<int>(), It.IsAny<CancellationToken>())).ReturnsAsync(new ProductDal() { Id = 1, Name = "Deleted_FromMockRepository_ProductObj" });
+            var service = new ProductService(_repositoryMock.Object, _mappingServiceMock.Object);
+
+
+            var delRes = await service.DeleteAsync(1);
+            Assert.NotNull(delRes);
+            Assert.NotEqual(0, delRes.Id);
+            _repositoryMock.Verify(prm => prm.DeleteAsync(It.IsAny<int>(), It.IsAny<CancellationToken>()));
+            _mappingServiceMock.Verify(pms => pms.MapAsync(It.IsAny<ProductDal>(), It.IsAny<CancellationToken>()));
+        }
+
+        [Theory]
+        [InlineData(404)]
+        public async Task DeleteAsync_Thrown_ArgumentException_Test(int id)
+        {
+            _repositoryMock.Setup(o => o.DeleteAsync(id, It.IsAny<CancellationToken>())).Throws(new ArgumentException());
+            var service = new ProductService(_repositoryMock.Object, _mappingServiceMock.Object);
+
+            await Assert.ThrowsAsync<ArgumentException>(async () => await service.DeleteAsync(id));
+            _repositoryMock.Verify(prm => prm.DeleteAsync(id, It.IsAny<CancellationToken>()));
+        }
+
+        [Fact]
+        public void GetTest()
+        {
+            _mappingServiceMock.Setup(mapper => mapper.Map(It.IsAny<ProductDal>())).Returns(TestData.TestData.ProductDom_1);
+            _repositoryMock.Setup(repo => repo.Get(It.IsAny<int>())).Returns(TestData.TestData.ProductDal_1);
+
+            var service = new ProductService(_repositoryMock.Object, _mappingServiceMock.Object);
+
+            var res = service.Get(1);
+
+            Assert.NotNull(res);
+            Assert.Equal(TestData.TestData.ProductDom_1.Id, res.Id);
+            Assert.Equal(TestData.TestData.ProductDom_1.Name, res.Name);
+            _repositoryMock.Verify(repo => repo.Get(It.IsAny<int>()));
+            _mappingServiceMock.Verify(mapper => mapper.Map(It.IsAny<ProductDal>()));
+        }
+
+        [Theory]
+        [InlineData(404)]
+        public void Get_Thrown_ArgumentException_Test(int id)
+        {
+            _repositoryMock.Setup(repo => repo.Get(id)).Throws(new ArgumentException());
+            var service = new ProductService(_repositoryMock.Object, _mappingServiceMock.Object);
+
+            Assert.Throws<ArgumentException>(() => service.Get(404));
+            _repositoryMock.Verify(repo => repo.Get(id));
+        }
+
+        [Fact]
+        public async Task GetAsyncTest()
+        {
+            _mappingServiceMock.Setup(mapper => mapper.MapAsync(It.IsAny<ProductDal>(), It.IsAny<CancellationToken>())).ReturnsAsync(TestData.TestData.ProductDom_1);
+            _repositoryMock.Setup(repo => repo.GetAsync(It.IsAny<int>(), It.IsAny<CancellationToken>())).ReturnsAsync(TestData.TestData.ProductDal_1);
+
+            var service = new ProductService(_repositoryMock.Object, _mappingServiceMock.Object);
+
+            var res = await service.GetAsync(1);
+
+            Assert.NotNull(res);
+            Assert.Equal(TestData.TestData.ProductDom_1.Id, res.Id);
+            Assert.Equal(TestData.TestData.ProductDom_1.Name, res.Name);
+            _repositoryMock.Verify(repo => repo.GetAsync(It.IsAny<int>(), It.IsAny<CancellationToken>()));
+            _mappingServiceMock.Verify(mapper => mapper.MapAsync(It.IsAny<ProductDal>(), It.IsAny<CancellationToken>()));
+        }
+
+        [Theory]
+        [InlineData(404)]
+        public async Task GetAsync_Thrown_ArgumentException_Test(int id)
+        {
+            _repositoryMock.Setup(repo => repo.GetAsync(id, It.IsAny<CancellationToken>())).ThrowsAsync(new ArgumentException());
+            var service = new ProductService(_repositoryMock.Object, _mappingServiceMock.Object);
+
+            await Assert.ThrowsAsync<ArgumentException>(async () => await service.GetAsync(404));
+            _repositoryMock.Verify(repo => repo.GetAsync(id, It.IsAny<CancellationToken>()));
+        }
+
+        [Fact]
+        public void GetAllTest()
+        {
+            _repositoryMock.Setup(repo => repo.GetAll()).Returns(_fixture.ProductsDal);
+            _mappingServiceMock.Setup(mapper => mapper.MapRange(_fixture.ProductsDal)).Returns(_fixture.ProductsDom);
+
+            var service = new ProductService(_repositoryMock.Object, _mappingServiceMock.Object);
+
+            var res = service.GetAll();
+
+            Assert.NotEqual(0, res.Count);
+            _repositoryMock.Verify(repo => repo.GetAll());
+            _mappingServiceMock.Verify(mapper => mapper.MapRange(_fixture.ProductsDal));
+        }
+
+        [Fact]
+        public async Task GetAllAsyncTest()
+        {
+            _repositoryMock.Setup(repo => repo.GetAllAsync(It.IsAny<CancellationToken>())).ReturnsAsync(_fixture.ProductsDal);
+            _mappingServiceMock.Setup(mapper => mapper.MapRangeAsync(_fixture.ProductsDal, It.IsAny<CancellationToken>())).ReturnsAsync(_fixture.ProductsDom);
+
+            var service = new ProductService(_repositoryMock.Object, _mappingServiceMock.Object);
+
+            var res = await service.GetAllAsync();
+
+            Assert.NotEqual(0, res.Count);
+            _repositoryMock.Verify(repo => repo.GetAllAsync(It.IsAny<CancellationToken>()));
+            _mappingServiceMock.Verify(mapper => mapper.MapRangeAsync(_fixture.ProductsDal, It.IsAny<CancellationToken>()));
+        }
+
+        [Theory]
+        [InlineData(1, "ItemToUpdate")]
+        public void UpdateTest(int id, string newName)
+        {
+            _mappingServiceMock.Setup(mapper => mapper.Map(It.IsAny<ProductDom>())).Returns(new ProductDal());
+            _repositoryMock.Setup(repo => repo.Update(id, It.IsAny<ProductDal>())).Returns(new ProductDal() { Id = id, Name = newName });
+            _mappingServiceMock.Setup(mapper => mapper.Map(It.IsAny<ProductDal>())).Returns(new ProductDom() { Id = id, Name = newName });
+
+            var service = new ProductService(_repositoryMock.Object, _mappingServiceMock.Object);
+            var res = service.Update(id, new ProductDom());
+
+            Assert.Equal(id, res.Id);
+            Assert.Equal(newName, res.Name);
+            _mappingServiceMock.Verify(mapper => mapper.Map(It.IsAny<ProductDom>()));
+            _repositoryMock.Verify(repo => repo.Update(id, It.IsAny<ProductDal>()));
+            _mappingServiceMock.Verify(mapper => mapper.Map(It.IsAny<ProductDal>()));
+        }
+
+        [Theory]
+        [InlineData(404)]
+        public void Update_Thrown_ArgumentException_Test(int id)
+        {
+            _mappingServiceMock.Setup(mapper => mapper.Map(It.IsAny<ProductDom>())).Returns(new ProductDal());
+            _repositoryMock.Setup(repo => repo.Update(id, It.IsAny<ProductDal>())).Throws(new ArgumentException());
+
+            var service = new ProductService(_repositoryMock.Object, _mappingServiceMock.Object);
+
+            Assert.Throws<ArgumentException>(()=> service.Update(id, new ProductDom()));
+
+            _mappingServiceMock.Verify(mapper => mapper.Map(It.IsAny<ProductDom>()));
+            _repositoryMock.Verify(repo => repo.Update(id, It.IsAny<ProductDal>()));
+        }
+
+        [Theory]
+        [InlineData(404)]
+        public void Update_Thrown_ArgumentNullException_Test(int id)
+        {
+            _mappingServiceMock.Setup(mapper => mapper.Map(default(ProductDom)!)).Returns(default(ProductDal)!);
+            _repositoryMock.Setup(repo => repo.Update(id, default(ProductDal)!)).Throws(new ArgumentNullException());
+
+            var service = new ProductService(_repositoryMock.Object, _mappingServiceMock.Object);
+
+            Assert.Throws<ArgumentNullException>(() => service.Update(id, null!));
+
+            _mappingServiceMock.Verify(mapper => mapper.Map(default(ProductDom)!));
+            _repositoryMock.Verify(repo => repo.Update(id, default(ProductDal)!));
+        }
+
+        [Theory]
+        [InlineData(1, "ItemToUpdateAsync")]
+        public async Task UpdateAsyncTest(int id, string newName)
+        {
+            _mappingServiceMock.Setup(mapper => mapper.MapAsync(It.IsAny<ProductDom>(), It.IsAny<CancellationToken>())).ReturnsAsync(new ProductDal());
+            _repositoryMock.Setup(repo => repo.UpdateAsync(id, It.IsAny<ProductDal>(), It.IsAny<CancellationToken>())).ReturnsAsync(new ProductDal() { Id = id, Name = newName });
+            _mappingServiceMock.Setup(mapper => mapper.MapAsync(It.IsAny<ProductDal>(), It.IsAny<CancellationToken>())).ReturnsAsync(new ProductDom() { Id = id, Name = newName });
+
+            var service = new ProductService(_repositoryMock.Object, _mappingServiceMock.Object);
+            var res = await service.UpdateAsync(id, new ProductDom());
+
+            Assert.Equal(id, res.Id);
+            Assert.Equal(newName, res.Name);
+            _mappingServiceMock.Verify(mapper => mapper.MapAsync(It.IsAny<ProductDom>(), It.IsAny<CancellationToken>()));
+            _repositoryMock.Verify(repo => repo.UpdateAsync(id, It.IsAny<ProductDal>(), It.IsAny<CancellationToken>()));
+            _mappingServiceMock.Verify(mapper => mapper.MapAsync(It.IsAny<ProductDal>(), It.IsAny<CancellationToken>()));
+        }
+
+        [Theory]
+        [InlineData(404)]
+        public async Task UpdateAsync_Thrown_ArgumentException_Test(int id)
+        {
+            _mappingServiceMock.Setup(mapper => mapper.MapAsync(It.IsAny<ProductDom>(), It.IsAny<CancellationToken>())).ReturnsAsync(new ProductDal());
+            _repositoryMock.Setup(repo => repo.UpdateAsync(id, It.IsAny<ProductDal>(), It.IsAny<CancellationToken>())).ThrowsAsync(new ArgumentException());
+
+            var service = new ProductService(_repositoryMock.Object, _mappingServiceMock.Object);
+
+            await Assert.ThrowsAsync<ArgumentException>(async () => await service.UpdateAsync(id, new ProductDom()));
+
+            _mappingServiceMock.Verify(mapper => mapper.MapAsync(It.IsAny<ProductDom>(), It.IsAny<CancellationToken>()));
+            _repositoryMock.Verify(repo => repo.UpdateAsync(id, It.IsAny<ProductDal>(), It.IsAny<CancellationToken>()));
+        }
+
+        [Theory]
+        [InlineData(404)]
+        public async Task UpdateAsync_Thrown_ArgumentNullException_Test(int id)
+        {
+            _mappingServiceMock.Setup(mapper => mapper.MapAsync(default(ProductDom)!, It.IsAny<CancellationToken>())).ReturnsAsync(default(ProductDal)!);
+            _repositoryMock.Setup(repo => repo.UpdateAsync(id, default(ProductDal)!, It.IsAny<CancellationToken>())).ThrowsAsync(new ArgumentNullException());
+
+            var service = new ProductService(_repositoryMock.Object, _mappingServiceMock.Object);
+
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => await service.UpdateAsync(id, null!));
+
+            _mappingServiceMock.Setup(mapper => mapper.MapAsync(default(ProductDom)!, It.IsAny<CancellationToken>()));
+            _repositoryMock.Setup(repo => repo.UpdateAsync(id, default(ProductDal)!, It.IsAny<CancellationToken>()));
         }
     }
 }
