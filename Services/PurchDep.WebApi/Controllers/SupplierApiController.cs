a2e5220b9b8aa26bbce75163b2635d4ad910ccd6
Aleksey N <nester.a.l@yandex.ru>
2022-08-13 15:17:44 +0300

    наследование контроллеров от нового базового контроллера

--- Services/PurchDep.WebApi/Controllers/SupplierApiController.cs
+++ Services/PurchDep.WebApi/Controllers/SupplierApiController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PurchDep.Dal.Entities;
 using PurchDep.Interfaces.Base.Services;
-using PurchDep.WebApi.Controllers.Base;
+using PurchDep.Interfaces.Base.Web;
 
 using SupplierDom = PurchDep.Domain.Supplier;
 
@@ -9,7 +9,7 @@ namespace PurchDep.WebApi.Controllers
 {
     [Route("api/suppliers")]
     [ApiController]
-    public class SupplierApiController : PurchDepBaseController<Supplier, SupplierDom>
+    public class SupplierApiController : PurchDepControllerBase<Supplier, SupplierDom>
     {
         public SupplierApiController(Service<Supplier, SupplierDom> service) : base(service) { }
     }
