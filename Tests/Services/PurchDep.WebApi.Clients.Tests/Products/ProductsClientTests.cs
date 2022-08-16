6f20f2e490ecb7e9ae97de297bfa5366e3b8e50e
Aleksey N <nester.a.l@yandex.ru>
2022-08-16 20:15:07 +0300

    интеграционное тестирование клиента складов, удаление лишнего проекта тестов

--- Tests/Services/PurchDep.WebApi.Clients.Tests/Products/ProductsClientTests.cs
+++ Tests/Services/PurchDep.WebApi.Clients.Tests/Products/ProductsClientTests.cs
@@ -1,10 +1,17 @@
 ﻿using Moq;
+using Moq.Protected;
+using PurchDep.Domain;
+using PurchDep.WebApi.Clients.Products;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace PurchDep.WebApi.Clients.Tests.Products
 {
@@ -12,7 +19,31 @@ namespace PurchDep.WebApi.Clients.Tests.Products
     {
         public ProductsClientTests()
         {
+
+        }
+
+        [Fact]
+        public async void Test1()
+        {
             var handlerMock = new Mock<HttpMessageHandler>();
+            var response = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(JsonSerializer.Serialize(new Product())),
+            };
+
+            handlerMock.Protected().Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>()).ReturnsAsync(response);
+            
+
+            var httpClient = new HttpClient(handlerMock.Object);
+
+            var client = new ProductsClient(httpClient);
+
+            var result = client.Get(1);
+
+            Assert.NotNull(result);
+            handlerMock.Protected().Setup<HttpResponseMessage>("Send", ItExpr.IsAny<HttpRequestMessage>());
+
         }
     }
 }
