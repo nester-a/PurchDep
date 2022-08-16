dc31d7aa8b4b06b090c71952c66ce5461ce4e26f
Aleksey N <nester.a.l@yandex.ru>
2022-08-16 18:07:42 +0300

    подготовка к тестированию клиентов

---
+++ Tests/Services/PurchDep.WebApi.Clients.Tests/Products/ProductsClientTests.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurchDep.WebApi.Clients.Tests.Products
+{
+    public class ProductsClientTests
+    {
+        public ProductsClientTests()
+        {
+            var handlerMock = new Mock<HttpMessageHandler>();
+        }
+    }
+}
