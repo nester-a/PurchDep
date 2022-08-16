64eee758c9c4be4e9e735fe9fd50381479c4dba5
Aleksey N <nester.a.l@yandex.ru>
2022-08-16 20:31:51 +0300

    контроллер складов для проекта мвц и его вьюшки

---
+++ UI/PurchDep.UI.Mvc/Controllers/StocksController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PurchDep.Domain;
+using PurchDep.Interfaces.Base.Services;
+
+namespace PurchDep.UI.Mvc.Controllers
+{
+    public class StocksController : Controller
+    {
+        private readonly IService<Stock> _service;
+
+        public StocksController(IService<Stock> service)
+        {
+            _service = service;
+        }
+
+
+        public IActionResult Index()
+        {
+            var items = _service.GetAll();
+            return View(items);
+        }
+        public IActionResult Details(int id)
+        {
+            var item = _service.Get(id);
+            return View(item);
+        }
+        public IActionResult Delete(int id)
+        {
+            _service.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
