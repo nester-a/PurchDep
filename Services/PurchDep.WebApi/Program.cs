dea4e9abc0b2518c0909a858ab1327e633914eb9
Aleksey N <nester.a.l@yandex.ru>
2022-08-17 16:06:31 +0300

    развернул проект веб-апи в докере

--- Services/PurchDep.WebApi/Program.cs
+++ Services/PurchDep.WebApi/Program.cs
@@ -41,13 +41,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    using (var scope = app.Services.CreateScope())
-    {
-        PurchDepContext context = scope.ServiceProvider.GetRequiredService<PurchDepContext>();
-        DbInitializer.Initialize(context);
-    }
-    app.UseSwagger();
-    app.UseSwaggerUI();
+}
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+using (var scope = app.Services.CreateScope())
+{
+    PurchDepContext context = scope.ServiceProvider.GetRequiredService<PurchDepContext>();
+    DbInitializer.Initialize(context);
 }
 
 app.UseAuthorization();
