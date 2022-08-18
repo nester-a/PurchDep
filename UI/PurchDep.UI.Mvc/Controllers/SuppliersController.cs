8409d0ccb73e7fbaf381b6b4714cebd90a9c812a
Aleksey N <nester.a.l@yandex.ru>
2022-08-18 17:53:38 +0300

    сделан метод и вью для добавления нового продукта поставщика

--- UI/PurchDep.UI.Mvc/Controllers/SuppliersController.cs
+++ UI/PurchDep.UI.Mvc/Controllers/SuppliersController.cs
@@ -6,29 +6,30 @@ namespace PurchDep.UI.Mvc.Controllers
 {
     public class SuppliersController : Controller
     {
-        private readonly IService<Supplier> _service;
+        private readonly IService<Supplier> _supplierService;
+        private readonly IService<Product> _productService;
 
-        public SuppliersController(IService<Supplier> service)
+        public SuppliersController(IService<Supplier> supplierService, IService<Product> productService)
         {
-            _service = service;
+            _supplierService = supplierService;
+            _productService = productService;
         }
 
         public IActionResult Index()
         {
-            var items = _service.GetAll();
+            var items = _supplierService.GetAll();
             return View(items);
         }
         public IActionResult Details(int id)
         {
-            var item = _service.Get(id);
+            var item = _supplierService.Get(id);
             return View(item);
         }
         public IActionResult Delete(int id)
         {
-            _service.Delete(id);
+            _supplierService.Delete(id);
             return RedirectToAction(nameof(Index));
         }
-
 
         public IActionResult Create()
         {
@@ -39,7 +40,7 @@ namespace PurchDep.UI.Mvc.Controllers
         {
             if (id is null) return View(new Supplier());
 
-            var supplier = _service.Get((int)id);
+            var supplier = _supplierService.Get((int)id);
             if (supplier is null) return NotFound();
 
             var item = new Supplier()
@@ -54,9 +55,36 @@ namespace PurchDep.UI.Mvc.Controllers
         public IActionResult Update(Supplier model)
         {
             var item = model;
-            if (item.Id == 0) _service.Add(item);
-            else _service.Update(item.Id, item);
+            if (item.Id == 0) _supplierService.Add(item);
+            else _supplierService.Update(item.Id, item);
 
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult AddSuppliersProduct(int id)
+        {
+            var model = new SuppliersProduct() { SupplierId = id };
+            return View("AddSuppliersProduct", model );
+        }
+
+        [HttpPost]
+        public IActionResult AddSuppliersProduct(SuppliersProduct supplierProduct)
+        {
+            if (supplierProduct is null) throw new ArgumentNullException();
+            if (supplierProduct.Id == 0)
+            {
+                var productDom = new Product()
+                {
+                    Name = supplierProduct.Name,
+                };
+                var res = _productService.Add(productDom);
+                supplierProduct.Id = res.Id;
+            }
+
+            var supplier = _supplierService.Get(supplierProduct.SupplierId);
+            supplier.SuppliersProducts.Add(supplierProduct);
+
+            _supplierService.Update(supplier.Id, supplier);
             return RedirectToAction(nameof(Index));
         }
     }
