0a70da33d1c0fb499d616bb9eadb653d425f243e
Aleksey N <nester.a.l@yandex.ru>
2022-08-19 16:00:10 +0300

    добавил несколько моделей для работы с контроллером поставщиков

---
+++ UI/PurchDep.UI.Mvc/Models/UpdateSuppliersProductModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PurchDep.UI.Mvc.Models
+{
+    public class UpdateSuppliersProductModel
+    {
+        public int ProductId { get; set; }
+        public int SupplierId { get; set; }
+        public decimal NewPrice { get; set; }
+    }
+}
