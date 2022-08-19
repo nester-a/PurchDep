858a85c76202afe3b6472182482738a6d88545eb
Aleksey N <nester.a.l@yandex.ru>
2022-08-19 16:09:34 +0300

    методы апдэйт и эдд для контроллера складов

--- UI/PurchDep.UI.Mvc/Controllers/StocksController.cs
+++ UI/PurchDep.UI.Mvc/Controllers/StocksController.cs
@@ -29,5 +29,36 @@ namespace PurchDep.UI.Mvc.Controllers
             _service.Delete(id);
             return RedirectToAction(nameof(Index));
         }
+
+
+        public IActionResult Create()
+        {
+            return View("Update", new Stock());
+        }
+
+        public IActionResult Update(int? id)
+        {
+            if (id is null) return View(new Stock());
+
+            var supplier = _service.Get((int)id);
+            if (supplier is null) return NotFound();
+
+            var item = new Stock()
+            {
+                Name = supplier.Name,
+            };
+
+            return View(item);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Stock model)
+        {
+            var item = model;
+            if (item.Id == 0) _service.Add(item);
+            else _service.Update(item.Id, item);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
