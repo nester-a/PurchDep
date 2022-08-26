2f3193ce11933dd62ff78de8354e3bc3e982dafe
Aleksey N <nester.a.l@yandex.ru>
2022-08-26 15:00:27 +0300

    рефакторинг хоум-контроллера и его представлений

--- UI/PurchDep.UI.Mvc/Controllers/HomeController.cs
+++ UI/PurchDep.UI.Mvc/Controllers/HomeController.cs
@@ -1,21 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PurchDep.Domain;
+using PurchDep.Domain.Base.Core;
 using PurchDep.Interfaces.Base.Services;
+using PurchDep.UI.Mvc.Models;
 
 namespace PurchDep.UI.Mvc.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IService<Product> service;
+        private readonly IService<Stock> _stockService;
+        private readonly IService<Supplier> _supplierService;
+        private readonly IService<Product> _productService;
 
-        public HomeController(IService<Product> service)
+        public HomeController(IService<Stock> stockService, IService<Supplier> supplierService, IService<Product> productService)
         {
-            this.service = service;
+            _stockService = stockService;
+            _supplierService = supplierService;
+            _productService = productService;
         }
         public IActionResult Index()
         {
-            var res = service.GetAll();
-            return View(res);
+            IndexNamedEnitiesListModel model = new();
+
+            List<IndexNamedEntityModel> models = new();
+            IndexNamedEntityModel products = new IndexNamedEntityModel()
+            {
+                NamedEntityName = "Products",
+            };
+            products.Items = _productService.GetAll();
+
+            IndexNamedEntityModel suppliers = new IndexNamedEntityModel()
+            {
+                NamedEntityName = "Suppliers",
+            };
+            suppliers.Items = _supplierService.GetAll();
+
+            //IndexNamedEntityModel stocks = new IndexNamedEntityModel()
+            //{
+            //    NamedEntityName = "Stocks",
+            //};
+            //stocks.Items = _stockService.GetAll();
+
+            models.Add(products);
+            models.Add(suppliers);
+
+            model.Items = models;
+
+            return View(model);
         }
     }
 }
