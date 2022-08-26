1287dba16896a3f1fe37bc656f881b5355c4af19
Aleksey N <nester.a.l@yandex.ru>
2022-08-26 12:53:06 +0300

    новый визуальный компонент для отображения именованных сущностей

---
+++ UI/PurchDep.UI.Mvc/Components/NamedEntitiesListViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PurchDep.Domain.Base.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurchDep.UI.Mvc.Components
+{
+    public class NamedEntitiesListViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(IEnumerable<INamedEntity> items)
+        {
+            var res = items.Take(5);
+
+            return View(res);
+        }
+    }
+}
--- UI/PurchDep.UI.Mvc/Controllers/HomeController.cs
+++ UI/PurchDep.UI.Mvc/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PurchDep.Domain;
+using PurchDep.Interfaces.Base.Services;
 
 namespace PurchDep.UI.Mvc.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IService<Product> service;
+
+        public HomeController(IService<Product> service)
+        {
+            this.service = service;
+        }
         public IActionResult Index()
         {
-            return View();
+            var res = service.GetAll();
+            return View(res);
         }
     }
 }
